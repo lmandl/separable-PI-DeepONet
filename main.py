fafc7fe230937253e3c27354a49efb2c492e764d
lmandl <githubluis@team-mandl.de>
2024-02-09 15:08:10 +0000

    extended code to multiple outputs with options to split trunk and branch net

--- main.py
+++ main.py
@@ -52,26 +52,51 @@ def main(args):
 
     print("Data assembled. Initializing model...")
 
+    # Overriding split_trunk and split_branch if num_outputs is 1
+    if args.num_outputs == 1:
+        args.split_trunk = False
+        args.split_branch = False
+
+    # Throw error if split_trunk and split_branch are both False and num_outputs is greater than 1
+    if not args.split_trunk and not args.split_branch and args.num_outputs > 1:
+        raise ValueError('split_trunk and split_branch cannot both be False when num_outputs > 1')
+
     # Initialize model and params
     # make sure trunk_layers and branch_layers are lists
     args.trunk_layers = [args.trunk_layers] if isinstance(args.trunk_layers, int) else args.trunk_layers
     args.branch_layers = [args.branch_layers] if isinstance(args.branch_layers, int) else args.branch_layers
 
     # add input and output features to trunk and branch layers
-    trunk_layers = [args.trunk_input_features]+args.trunk_layers+[args.hidden_dim]
+    # split trunk if split_trunk is True
+    if args.split_trunk:
+        trunk_layers = [args.trunk_input_features]+args.trunk_layers+[args.num_outputs*args.hidden_dim]
+    else:
+        trunk_layers = [args.trunk_input_features]+args.trunk_layers+[args.hidden_dim]
 
     # Stacked or unstacked DeepONet
+    # dimensions for splits are calculated once here
     if args.stacked_do:
         # add input and output features to branch layers for stacked DeepONet, which has one output feature
+        # if split_branch is True, the output features are split into n groups for n outputs but layer sizes are kept
         branch_layers = [args.branch_input_features]+args.branch_layers+[1]
         # build model
-        model = StackedDeepONet(branch_layers, trunk_layers, args.num_outputs, args.hidden_dim)
+        if args.split_branch:
+            # If branches are split, we need to multiply the hidden_dim by the number of outputs
+            model = StackedDeepONet(branch_layers, trunk_layers, args.split_branch, args.split_trunk,
+                                    args.num_outputs, args.hidden_dim * args.num_outputs)
+        else:
+            model = StackedDeepONet(branch_layers, trunk_layers, args.split_branch, args.split_trunk,
+                                    args.num_outputs, args.hidden_dim)
     else:
         # add input and output features to branch layers for unstacked DeepONet
-        branch_layers = [args.branch_input_features]+args.branch_layers+[args.hidden_dim]
+        # if split_branch is True, the output features are split into n groups for n outputs
+        if args.split_branch:
+            branch_layers = [args.branch_input_features]+args.branch_layers+[args.num_outputs*args.hidden_dim]
+        else:
+            branch_layers = [args.branch_input_features]+args.branch_layers+[args.hidden_dim]
         # build model
-        model = UnstackedDeepONet(branch_layers, trunk_layers, args.num_outputs)
-
+        model = UnstackedDeepONet(branch_layers, trunk_layers, args.split_branch, args.split_trunk,
+                                  args.num_outputs)
     # Initialize parameters
     params = model.init(key, jnp.ones(args.branch_input_features), jnp.ones(args.trunk_input_features))
 
@@ -131,7 +156,8 @@ if __name__ == '__main__':
                                  "t_end": 1},
                         help='hyperparameters for equation setup, e.g. domain, ranges, boundary conditions, etc.')
     parser.add_argument('--num_outputs', type=int, default=1, help='number of outputs')
-    parser.add_argument('--hidden_dim', type=int, default=4, help='latent layer size in DeepONet, also called >>p<<')
+    parser.add_argument('--hidden_dim', type=int, default=20,
+                        help='latent layer size in DeepONet, also called >>p<<, multiples are used for splits')
     parser.add_argument('--stacked_deeponet', dest='stacked_do', default=False, action='store_true',
                         help='use stacked DeepONet, if false use unstacked DeepONet')
 
@@ -150,10 +176,16 @@ if __name__ == '__main__':
     parser.add_argument('--branch_layers', type=int, nargs="+", default=40, help='hidden branch layer sizes')
     parser.add_argument('--branch_input_features', type=int, default=100,
                         help='number of input features to branch network')
+    parser.add_argument('--split_branch', dest='split_branch', default=True, action='store_false',
+                        help='split branch outputs into n groups for n outputs')
+    # TODO: later change to default=False
 
     # Trunk settings
     parser.add_argument('--trunk_layers', type=int, nargs="+", default=40, help='hidden trunk layer sizes')
     parser.add_argument('--trunk_input_features', type=int, default=1, help='number of input features to trunk network')
+    parser.add_argument('--split_trunk', dest='split_trunk', default=True, action='store_false',
+                        help='split trunk outputs into n groups for n outputs')
+    # TODO: later change to default=False
 
     # Training settings
     parser.add_argument('--seed', type=int, default=1337, help='random seed')
--- models/deeponet.py
+++ models/deeponet.py
@@ -6,12 +6,20 @@ from typing import Sequence
 class UnstackedDeepONet(nn.Module):
     branch_layers: Sequence[int]
     trunk_layers: Sequence[int]
-    output_dim: int
+    split_branch: bool = False
+    split_trunk: bool = False
+    output_dim: int = 1
 
     @nn.compact
     def __call__(self, branch_x, trunk_x):
 
         init = nn.initializers.glorot_normal()
+
+        # Make sure the input is 2D (also during init)
+        if len(branch_x.shape) == 1:
+            branch_x = jnp.reshape(branch_x, (1, -1))
+        if len(trunk_x.shape) == 1:
+            trunk_x = jnp.reshape(trunk_x, (1, -1))
 
         # Branch network
         for i, fs in enumerate(self.branch_layers[:-1]):
@@ -20,13 +28,22 @@ class UnstackedDeepONet(nn.Module):
         branch_x = nn.Dense(self.branch_layers[-1], name=f"branch_{i+1}", kernel_init=init)(branch_x)
         # no output activation
 
+        # reshape the output
+        # reshape from [batch_size, p*output_dim] to [batch_size, p, output_dim] if split_branch is True
+        if self.split_branch:
+            branch_x = jnp.reshape(branch_x, (-1, branch_x.shape[1] // self.output_dim, self.output_dim))
+
         # Trunk network
         for i, fs in enumerate(self.trunk_layers):
             trunk_x = nn.Dense(fs, kernel_init=init, name=f"trunk_{i}")(trunk_x)
             trunk_x = nn.activation.tanh(trunk_x)
+        # reshape the output
+        # reshape from [batch_size, p*output_dim] to [batch_size, p, output_dim] if split_trunk is True
+        if self.split_trunk:
+            trunk_x = jnp.reshape(trunk_x, (-1, trunk_x.shape[1] // self.output_dim, self.output_dim))
 
         # Compute the final output
-        result = jnp.sum(branch_x*trunk_x, axis=-1)
+        result = output_einsum(self.split_branch, self.split_trunk, branch_x, trunk_x)
 
         # Add bias
         bias = self.param('output_bias', nn.initializers.zeros, (self.output_dim,))
@@ -38,17 +55,25 @@ class UnstackedDeepONet(nn.Module):
 class StackedDeepONet(nn.Module):
     branch_layers: Sequence[int]
     trunk_layers: Sequence[int]
-    output_dim: int
-    hidden_dim: int
+    split_branch: bool = False
+    split_trunk: bool = False
+    output_dim: int = 1
+    n_branches: int = 1
 
     @nn.compact
     def __call__(self, branch_x, trunk_x):
 
         init = nn.initializers.glorot_normal()
 
+        # Make sure the input is 2D (also during init)
+        if len(branch_x.shape) == 1:
+            branch_x = jnp.reshape(branch_x, (1, -1))
+        if len(trunk_x.shape) == 1:
+            trunk_x = jnp.reshape(trunk_x, (1, -1))
+
         # Branch networks
         branch_out = []
-        for j in range(self.hidden_dim):
+        for j in range(self.n_branches):
             for i, fs in enumerate(self.branch_layers[:-1]):
                 branch_x = nn.Dense(fs, kernel_init=init, name=f"branch_{j}_{i}")(branch_x)
                 branch_x = nn.activation.tanh(branch_x)
@@ -56,19 +81,44 @@ class StackedDeepONet(nn.Module):
             # no output activation
             branch_out.append(branch_x)
 
+        # transform list of the output into [batch_size, p*output_dim]
         # Combine the outputs of each branch into a JAX array
-        branch_out = jnp.stack(branch_out, axis=-1).squeeze()
+        branch_x = jnp.reshape(jnp.array(branch_out), (-1, len(branch_out)))
+        # reshape from [batch_size, p*output_dim] to [batch_size, p, output_dim] if split_branch is True
+        if self.split_branch:
+            branch_x = jnp.reshape(branch_x, (-1, branch_x.shape[1] // self.output_dim, self.output_dim))
 
         # Trunk network
         for i, fs in enumerate(self.trunk_layers):
             trunk_x = nn.Dense(fs, kernel_init=init, name=f"trunk_{i}")(trunk_x)
             trunk_x = nn.activation.tanh(trunk_x)
+        # reshape the output
+        # reshape from [batch_size, p*output_dim] to [batch_size, p, output_dim] if split_trunk is True
+        if self.split_trunk:
+            trunk_x = jnp.reshape(trunk_x, (-1, trunk_x.shape[1] // self.output_dim, self.output_dim))
 
         # Compute the final output
-        result = jnp.sum(branch_out * trunk_x, axis=-1)
+        result = output_einsum(self.split_branch, self.split_trunk, branch_x, trunk_x)
 
         # Add bias
         bias = self.param('output_bias', nn.initializers.zeros, (self.output_dim,))
         result += bias
 
         return result
+
+
+def output_einsum(split_branch, split_trunk, branch, trunk):
+    # Input shapes:
+    # branch: [batch_size, p, output_dim] if split_branch is True
+    # branch: [batch_size, output_dim] if split_branch is False
+    # trunk: [batch_size, p, output_dim] if split_trunk is True
+    # trunk: [batch_size, output_dim] if split_trunk is False
+    # output: [batch_size, output_dim]
+    if split_branch and split_trunk:
+        return jnp.einsum('ijk,ijk->ik', branch, trunk)
+    elif split_branch and not split_trunk:
+        return jnp.einsum('ijk,ij->ik', branch, trunk)
+    elif split_trunk and not split_branch:
+        return jnp.einsum('ij,ijk->ik', branch, trunk)
+    else:
+        return jnp.einsum('ij,ij->i', branch, trunk)
